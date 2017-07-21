3ccce56bbb08486bed04b04749a45c58ab0527b2
Thành Bùi Văn <Thnh Bi Vn>
2017-07-21 15:49:22 +0700

    Add project files.

---
+++ Scannn/Scannn.Android/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using ZXing.Net.Mobile.Forms;
+
+namespace Scannn.Droid
+{
+    [Activity(Label = "Scannn", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ Scannn/Scannn/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using ZXing.Net.Mobile.Forms;
+
+namespace Scannn
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var buttonScan = this.FindByName<Button>("BtnScan");
+            buttonScan.Clicked += async delegate {
+                var scanPage = new ZXingScannerPage();
+                scanPage.OnScanResult += (result) => {
+                    // Stop scanning
+                    scanPage.IsScanning = false;
+
+                    // Pop the page and show the result
+                    Device.BeginInvokeOnMainThread(() => {
+                        Navigation.PopAsync();
+                        DisplayAlert("Scanned Barcode", result.Text, "OK");
+                    });
+                };
+
+                await Navigation.PushAsync(scanPage);
+            };
+        }
+    }
+}
